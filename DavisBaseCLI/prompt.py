a41c0f0396d589bf0546c969bd019650b51ceb64
Kamalesh <kamalesh.anantha@gmail.com>
2019-11-20 12:04:58 -0600

    Created prompt.py
    Prompt.py file will deal with user input. SQL commands entered by the user will be validated and appropriate sub routines will be called based on the parsed tokens.
    
    defined help method

---
+++ DavisBaseCLI/prompt.py
@@ -0,0 +1,26 @@
+#Splash Screen
+
+print("Welcome to Davisbase")
+
+def help(): 
+	print("*" * 80)
+	print("SUPPORTED COMMANDS\n")
+	print("All commands below are case insensitive\n")
+	print("SHOW TABLES")
+	print("\tDisplay the names of all tables.\n")
+	print("SELECT * FROM <table_name>")
+	print("Display all records in the table <table_name>.\n")
+	print("SELECT <column_list> FROM <table_name> [WHERE <condition>]")
+	print("\tDisplay table records whose optional <condition>")
+	print("\tis <column_name> = <value>.\n")
+	print("DROP TABLE <table_name>")
+	print("\tRemove table data (i.e. all records) and its schema.\n")
+	print("UPDATE TABLE <table_name> SET <column_name> = <value> [WHERE <condition>]")
+	print("\tModify records data whose optional <condition> is\n")
+	print("VERSION")
+	print("\tDisplay the program version.\n")
+	print("HELP")
+	print("\tDisplay this help information.\n")
+	print("EXIT")
+	print("\tExit the program.\n")
+	print("*" * 80)     
